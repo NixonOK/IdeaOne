f6ef14f8071c176bfeca49b61a92ec5a96daebba
DESKTOP-9I814MI\Jyoti Roy <cse.roy.jyoti@gmail.com>
2016-10-31 21:29:58 +0600

    Verry demo!
    Testing!

---
+++ IdeaOne/Assets/Scripts/floorSelector.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Author: Comming Soon!
+ * Scripter: Jyoti Roy,  ,  ,  !
+ * Description: This script to select the floor
+ * 
+ */
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class floorSelector : MonoBehaviour {
+
+	public Sprite floorSampleOne, floorSampleTwo; //reference to the floor sprites
+
+
+
+
+	void Start () {
+
+		GetComponent<SpriteRenderer> ().sprite = floorSampleOne; // set a prefixed floor image
+	
+	}
+	
+
+
+
+
+	void Update () {
+	
+	}
+}
